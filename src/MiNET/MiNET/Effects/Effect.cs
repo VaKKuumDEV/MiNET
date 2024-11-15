95cd43d347c87f0c4e51fc628e15a49daa0d8f9e
laz1444 <robertsrihards@inbox.lv>
2024-11-15 19:09:02 +0200

    ...

--- src/MiNET/MiNET/Effects/Effect.cs
+++ src/MiNET/MiNET/Effects/Effect.cs
@@ -134,118 +134,118 @@ namespace MiNET.Effects
 			List<Effect> effect = new List<Effect>();
 			switch (Metadata)
 			{
-				case 5: // Splash Potion of Night Vision (3:00)
+				case 5: // Night Vision (3:00)
 					effect.Add(new NightVision { Duration = 3600 });
 					break;
-				case 6: // Splash Potion of Night Vision (8:00)
+				case 6: // Night Vision (8:00)
 					effect.Add(new NightVision { Duration = 9600 });
 					break;
-				case 7: // Splash Potion of Invisibility (3:00)
+				case 7: // Invisibility (3:00)
 					effect.Add(new Invisibility { Duration = 3600 });
 					break;
-				case 8: // Splash Potion of Invisibility (8:00)
+				case 8: // Invisibility (8:00)
 					effect.Add(new Invisibility { Duration = 9600 });
 					break;
-				case 9: // Splash Potion of Leaping (Jump Boost 1, 3:00)
+				case 9: // Leaping (Jump Boost 1, 3:00)
 					effect.Add(new JumpBoost { Duration = 3600 });
 					break;
-				case 10: // Splash Potion of Leaping (Jump Boost 1, 8:00)
+				case 10: // Leaping (Jump Boost 1, 8:00)
 					effect.Add(new JumpBoost { Duration = 9600 });
 					break;
-				case 11: // Splash Potion of Leaping (Jump Boost 2, 1:30)
+				case 11: // Leaping (Jump Boost 2, 1:30)
 					effect.Add(new JumpBoost { Duration = 2600, Level = 1 });
 					break;
-				case 12: // Splash Potion of Fire Resistance (3:00)
+				case 12: // Fire Resistance (3:00)
 					effect.Add(new FireResistance { Duration = 3600 });
 					break;
-				case 13: // Splash Potion of Fire Resistance (8:00)
+				case 13: // Fire Resistance (8:00)
 					effect.Add(new FireResistance { Duration = 9600 });
 					break;
-				case 14: // Splash Potion of Swiftness (Speed 1, 3:00)
+				case 14: // Swiftness (Speed 1, 3:00)
 					effect.Add(new Speed { Duration = 3600 });
 					break;
-				case 15: // Splash Potion of Swiftness (Speed 1, 8:00)
+				case 15: // Swiftness (Speed 1, 8:00)
 					effect.Add(new Speed { Duration = 9600 });
 					break;
-				case 16: // Splash Potion of Swiftness (Speed 2, 1:30)
+				case 16: // Swiftness (Speed 2, 1:30)
 					effect.Add(new Speed { Duration = 1800, Level = 1 });
 					break;
-				case 17: // Splash Potion of Slowness (1:30)
+				case 17: // Slowness (1:30)
 					effect.Add(new Slowness { Duration = 1800 });
 					break;
-				case 18: // Splash Potion of Slowness (4:00)
+				case 18: // Slowness (4:00)
 					effect.Add(new Slowness { Duration = 4800 });
 					break;
-				case 19: // Splash Potion of Water Breathing (3:00)
+				case 19: // Water Breathing (3:00)
 					effect.Add(new WaterBreathing { Duration = 3600 });
 					break;
-				case 20: // Splash Potion of Water Breathing (8:00)
+				case 20: // Water Breathing (8:00)
 					effect.Add(new WaterBreathing { Duration = 9600 });
 					break;
-				case 21: // Splash Potion of Healing
+				case 21: // Healing
 					effect.Add(new InstantHealth { });
 					break;
-				case 22: // Splash Potion of healing 2
+				case 22: // healing 2
 					effect.Add(new InstantHealth { Level = 1 });
 					break;
-				case 23: // Splash Potion of Harming
+				case 23: // Harming
 					effect.Add(new InstantDamage { });
 					break;
-				case 24: // Splash Potion of Harming 2
+				case 24: // Harming 2
 					effect.Add(new InstantDamage { Level = 1 });
 					break;
-				case 25: // Splash Potion of Poison (Poison 1, 0:45)
+				case 25: // Poison (Poison 1, 0:45)
 					effect.Add(new Poison { Duration = 900 });
 					break;
-				case 26: // Splash Potion of Poison (Poison 1, 2:00)
+				case 26: // Poison (Poison 1, 2:00)
 					effect.Add(new Poison { Duration = 2400 });  //test
 					break;
-				case 27: // Splash Potion of Poison (Poison 2, 0:22)
+				case 27: // Poison (Poison 2, 0:22)
 					effect.Add(new Poison { Duration = 440, Level = 1 });
 					break;
-				case 28: // Splash Potion of Regeneration (Regen 1, 0:45)
+				case 28: // Regeneration (Regen 1, 0:45)
 					effect.Add(new Regeneration { Duration = 900 });
 					break;
-				case 29: // Splash Potion of Regeneration (Regen 1, 2:00)
+				case 29: // Regeneration (Regen 1, 2:00)
 					effect.Add(new Regeneration { Duration = 2400 });  //test
 					break;
-				case 30: // Splash Potion of Regeneration (Regen 2, 0:22)
+				case 30: // Regeneration (Regen 2, 0:22)
 					effect.Add(new Regeneration { Duration = 440, Level = 1 });
 					break;
-				case 31: // Splash Potion of Strength (Strength 1, 3:00)
+				case 31: // Strength (Strength 1, 3:00)
 					effect.Add(new Strength { Duration = 3600 });
 					break;
-				case 32: // Splash Potion of Strength (Strength 1, 8:00)
+				case 32: // Strength (Strength 1, 8:00)
 					effect.Add(new Strength { Duration = 9600 });
 					break;
-				case 33: // Splash Potion of Strength (Strength 2, 1:30)
+				case 33: // Strength (Strength 2, 1:30)
 					effect.Add(new Strength { Duration = 1800, Level = 1 });
 					break;
-				case 34: // Splash Potion of Weakness (1:30)
+				case 34: // Weakness (1:30)
 					effect.Add(new Weakness { Duration = 1800 });
 					break;
-				case 35: // Splash Potion of Weakness (4:00)
+				case 35: // Weakness (4:00)
 					effect.Add(new Weakness { Duration = 4800 });
 					break;
-				case 36: // Splash Potion of Decay (Wither 2, 0:40)
+				case 36: // Decay (Wither 2, 0:40)
 					effect.Add(new Wither { Duration = 800, Level = 1 });
 					break;
-				case 37: // Splash Potion of the Turtle Master (Slowness 4 & Resistance 3, 0:20)
+				case 37: //Turtle Master (Slowness 4 & Resistance 3, 0:20)
 					effect.Add(new Slowness { Duration = 400, Level = 3 });
 					effect.Add(new Resistance { Duration = 400, Level = 2 });
 					break;
-				case 38: // Splash Potion of the Turtle Master (Slowness 4 & Resistance 3, 0:40)
+				case 38: // Turtle Master (Slowness 4 & Resistance 3, 0:40)
 					effect.Add(new Slowness { Duration = 800, Level = 3 });
 					effect.Add(new Resistance { Duration = 800, Level = 2 });
 					break;
-				case 39: // Splash Potion of the Turtle Master (Slowness 6 & Resistance 4, 0:20)
+				case 39: // Turtle Master (Slowness 6 & Resistance 4, 0:20)
 					effect.Add(new Slowness { Duration = 400, Level = 5 });
 					effect.Add(new Resistance { Duration = 400, Level = 3 });
 					break;
-				case 40: // Splash Potion of Slow Falling (1:30)
+				case 40: // Slow Falling (1:30)
 					effect.Add(new SlowFalling { Duration = 1800 });
 					break;
-				case 41: // Splash Potion of Slow Falling (4:00)
+				case 41: // Slow Falling (4:00)
 					effect.Add(new SlowFalling { Duration = 4800 });
 					break;
 			}
